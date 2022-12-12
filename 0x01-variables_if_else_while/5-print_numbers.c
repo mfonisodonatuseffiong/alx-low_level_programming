7a91c61817c5ea8af50ea024cd6f746a39f920a3
mfonisodonatuseffiong <mfonisodonatus@gmail.com>
2022-12-11 16:08:06 -0800

    fgo

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	for (a = 0; a <= 10; a++)
+		printf("%d", a);
+	putchar('\n')
+	return (0);
+}
