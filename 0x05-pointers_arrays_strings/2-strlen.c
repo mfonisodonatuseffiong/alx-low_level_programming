fd8664847cda9083adfe26b50ba13d5a36abdea3
mfonisodonatuseffiong <mfonisodonatus@gmail.com>
2022-12-20 12:20:29 -0800

    kw

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,11 +7,18 @@
  */
 int _strlen(char *s)
 {
-	int i = 0;
 
-	while(S[i])
-	i++;
+	int i = 1, sum = 0;
+	char pl = s[0];
 
-	return (i);
+
+	while (pl != '\0')
+	{
+
+		sum++;
+		pl = s[i++];
+											}
+	return (sum);
+
 }
 	
