b49a0583ff67bd809fb9bcbd28aea0bef5057c0b
mfonisodonatuseffiong <mfonisodonatus@gmail.com>
2022-12-22 04:13:26 -0800

    threee

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,12 +9,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-
 	int i = 0, diff = 0;
 
 	for ( ; *(s1 + i) != '\n' ; i++)
 	{
-		
 		if (&s1 == &s2  || i > 509)
 		{
 			diff = 0;
@@ -22,7 +20,6 @@ int _strcmp(char *s1, char *s2)
 		}
 		diff = s1[i] - s2[i];
 		if (diff != 0)
-
 		     	break;
 	}
 	return (diff);
