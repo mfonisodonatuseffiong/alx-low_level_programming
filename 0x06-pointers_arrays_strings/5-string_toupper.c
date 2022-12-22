f32d8dcaa7b7d871eaed333a3725d01ed9ffb461
mfonisodonatuseffiong <mfonisodonatus@gmail.com>
2022-12-22 04:36:06 -0800

    five

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to upperrcase
+ * @a: char
+ * Return: char
+ */
+
+char *string_toupper(char *a)
+{
+
+	int i = 0;
+
+	while (a[i])
+	{
+
+		if (a[i] >= 97 && a[i] <= 122)
+		{
+			a[i] = a[i] - 32;
+		}
+		i++;
+	}
+	return (a);
+}
