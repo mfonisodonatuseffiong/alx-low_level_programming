bc1b5895408dca6a1359be8e1f91857a443a33c8
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2024-03-26 12:33:04 -0700

    liball

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,6 +1,2 @@
-
 #!/bin/bash
-gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
-gcc -shared -o liball.so *.o
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
-
+gcc -o *.c -fpic && gcc *o -shared -o liball.so
